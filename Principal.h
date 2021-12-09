2f83782994fb00796d97ec0942f546efabaf3f66
davidalejandrolazopampa <40555746+davidalejandrolazopampa@users.noreply.github.com>
2021-12-09 03:15:22 -0500

    rutas

--- Principal.h
+++ Principal.h
@@ -1,6 +1,12 @@
 #ifndef RECONSTRUCTION_3D_PROYECTO3_PRINCIPAL_H
 #define RECONSTRUCTION_3D_PROYECTO3_PRINCIPAL_H
-
+/*********************************************/
+#include <bits/stdc++.h>
+#include <GL/freeglut.h>
+#include <chrono>
+/*********************************************/
+#include "CImg.h"
+/*********************************************/
 #define F first;
 #define S second;
 #define PB push_back;
@@ -8,7 +14,7 @@
 #define FOR(i,a,b) for(long long i=(a);i <= b;i++)
 #define ROF(i,a,b) for(long long i=(a);i>=b;i--)
 #define sz(x) (int)(x).size();
-
+/*********************************************/
 using namespace std;
 //Todo typedef
 typedef long long ll;
@@ -23,5 +29,5 @@ typedef vector<bool> vb;
 //Todo depende del cambio de N
 const int N = 2e+5+10;
 int arr[N] ;
-
+/*********************************************/
 #endif //RECONSTRUCTION_3D_PROYECTO3_PRINCIPAL_H
