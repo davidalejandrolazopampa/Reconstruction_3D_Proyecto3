f6a14b8e53e0a5a505e34e504338582e56e2fb6d
davidalejandrolazopampa <40555746+davidalejandrolazopampa@users.noreply.github.com>
2021-12-19 17:04:36 -0500

    Vector_1 Modificado VEC3F

---
+++ vector_1.h
@@ -0,0 +1,12 @@
+#ifndef RECONSTRUCTION_3D_PROYECTO3_VECTOR_1_H
+#define RECONSTRUCTION_3D_PROYECTO3_VECTOR_1_H
+class vec3f{
+private:
+protected:
+public:
+     float x_,y_,z_ ;
+     vec3f(float _x, float _y, float _z):x_{_x}, y_{_y}, z_{_z}{}
+};
+
+
+#endif //RECONSTRUCTION_3D_PROYECTO3_VECTOR_1_H
