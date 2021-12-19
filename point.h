b75a65b7b859c00844443f77497f7fdd4a05e381
davidalejandrolazopampa <40555746+davidalejandrolazopampa@users.noreply.github.com>
2021-12-19 17:05:07 -0500

    Puntos

--- point.h
+++ point.h
@@ -1,25 +1,34 @@
-//
-// Created by davla on 12/12/2021.
-//
-
 #ifndef RECONSTRUCTION_3D_PROYECTO3_POINT_H
 #define RECONSTRUCTION_3D_PROYECTO3_POINT_H
-
 /*********************************************/
-#include "Principal.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <GL/freeglut.h>
 /*********************************************/
-
+#include "CImg.h"
+#include "vector_1.h"
+/*********************************************/
+using namespace cimg_library;
+using namespace std;
+/*********************************************/
+#define FOR(i,a,b) for(int i=(a);i<b;i++)
+/*********************************************/
 class Point {
 private:
 protected:
 public:
     vector<CImg <char>> picture_;
     vector<vector <vec3f>> point_;
-    int num_picture_, height, wide;
+    int num_picture_=0, height, wide;
 
     CImg <char> barrido_vector(CImg<float> &picturee, int cominezo) const;
     void verPicture(const string& road_picture, int cominezo);
-    void point_out();
+    void point_out(float distance_IMG);
+    void guardar_point(const string& camino);
+    void load_file(const string& camino_Point);
 };
 
 
